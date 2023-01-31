3dda7f98f73a2fe2c60cd204323ec0b7ce641d98
Rebecca Biju <biju.r@northeastern.edu>
2023-01-31 18:33:30 -0500

    Health test incomplete

--- api/tests/health.test.js
+++ api/tests/health.test.js
@@ -1,5 +1,11 @@
-describe('Sample Test', () => {
-    it('should test that true === true', () => {
-        expect(true).toBe(true)
+import request from "supertest";
+import app from "../app.js";
+
+
+describe('Health Endpoint', () => {
+    it('should hit the health endpoint /healthz', async() => {
+        const res = request(app).get('/healthz')
+        console.log(res)
+            // expect(res).toBe(200)
     })
 })
