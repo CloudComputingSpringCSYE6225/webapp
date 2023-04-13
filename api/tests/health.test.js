e7b2f0863834e08814c15faa2fd45fd26c82d04f
Rebecca Biju <113070179+beccccaboo@users.noreply.github.com>
2023-04-13 16:31:31 -0400

    Update health.test.js

--- api/tests/health.test.js
+++ api/tests/health.test.js
@@ -10,8 +10,8 @@ import app from "../app.js";
 // })
 
 describe("GET Health Endpoint", () => {
-    test("Check health on /health", async () => {
-        const res = await request(app).get("/healthz");
+    test("Check health on /healthz", async () => {
+        const res = await request(app).get("/health");
         expect(res.statusCode).toEqual(200);
     });
 });
